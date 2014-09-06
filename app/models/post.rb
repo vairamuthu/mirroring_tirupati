a2eae7c4e2c0532af8a7ddadb271629edb2230af
vairamuthu <vairamuthu.it@gmail.com>
2014-09-07 01:23:49 +0530

    add validation post image

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" },  :url => "/assets/posts/:id/:style/:basename.:extension",
   :path => ":rails_root/public/assets/posts/:id/:style/:basename.:extension"
-  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :photo, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/
   belongs_to :city
 end
